5b09021e1ed331a818aeb698e14b5cb810bcbca7
Ryan <46852695+ryanAllMad@users.noreply.github.com>
2022-12-23 12:25:43 -0800

    updating jasons fav color

--- src/components/Jason.jsx
+++ src/components/Jason.jsx
@@ -6,7 +6,7 @@ import Person from "./Person"
 const Jason = () => {
     const { retreatStatus, changeRetreatStatus } = useContext(PersonContext)
     return  (
-    <Person age="32" name="jason" favoriteColor="orange" hobbies="writing, making stacks" gender="male">
+    <Person age="32" name="jason" favoriteColor="black" hobbies="writing, making stacks" gender="male">
         <button onClick={changeRetreatStatus}>{retreatStatus}</button>
     </Person>
     )
